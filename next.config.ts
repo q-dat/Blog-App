4e71442f2120622a3622d30b3673e66849f7dee5
q-dat <quocdatstore79@gmail.com>
2025-05-08 13:49:19 +0700

    init source

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  devIndicators: false,
+  images: {
+    formats: ["image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-3xl text-red-500 bg-black font-bold underline">Hello world!</h1>;
+}
